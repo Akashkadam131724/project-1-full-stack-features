f4f8104022b720fc8b89bc24814bdd752192dea9
Akashkadam131724 <kadamakash131724@gmail.com>
2024-01-02 11:03:30 +0530

    added dotenv

--- db/conn.js
+++ db/conn.js
@@ -2,10 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(
-      "mongodb+srv://kadamakash131724:6qy1eSGupLMWDUeN@project-1-full-features.pwjigtr.mongodb.net/project-1?retryWrites=true&w=majority"
-    );
-    console.log("connecting to project Db");
+    await mongoose.connect(process.env.MONGO_URI);
   } catch (e) {
     console.log(err.message);
   }

aed23eb4c31139fb732984f5094456e9812e7cec
Akashkadam131724 <kadamakash131724@gmail.com>
2024-01-02 11:05:39 +0530

    message

--- routes/home.js
+++ routes/home.js
@@ -3,7 +3,7 @@ const getHomePage = require("../controllers/home");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.send("Welcome to home ");
+  res.send("Welcome to home added dotenv");
 });
 
 router.get("/home", getHomePage);
